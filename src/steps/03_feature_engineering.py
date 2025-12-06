f44971c91adbb4d236207d494f98c2c386faaec2
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2025-12-06 14:48:20 -0300

    feat: add feature engineering step 3

--- src/steps/03_feature_engineering.py
+++ src/steps/03_feature_engineering.py
@@ -1,1 +1,18 @@
 # * Imputed -> Final ML Dataset
+
+import logging
+
+from src.shared import config, utils, logger
+# from src.processing import analysis
+
+logger.setup_logging()
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("=== STEP 03: STARTING FEATURE ENGINEERING ===\n")
+
+    logger.info("=== STEP 03: COMPLETED SUCCESSFULLY ===\n")
+
+
+if __name__ == "__main__":
+    main()
