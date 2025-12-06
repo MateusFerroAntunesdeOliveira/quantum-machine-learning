47092d3286a5f8a0e3dacbb3d7233a362906026b
Mateus Ferro <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2025-12-06 15:01:13 -0300

    feat: add action plan to feature engineering step

--- src/steps/03_feature_engineering.py
+++ src/steps/03_feature_engineering.py
@@ -11,8 +11,26 @@ logger = logging.getLogger(__name__)
 def main():
     logger.info("=== STEP 03: STARTING FEATURE ENGINEERING ===\n")
 
-    logger.info("=== STEP 03: COMPLETED SUCCESSFULLY ===\n")
+    # 4 sub-steps:
+    
+    # Preparation.
+        # ABIDE uses 1 and 2 for groups. We need to change to 0 and 1 (binary).
 
+    # Feature Generation. Maybe QI * Age...
+        # But, we can not apply polynomial features to all of them. If we have 100 columns, we would have 1000+ features.
+        # So, apply polynomial features (degree=2) only to CORE ATTRIBUTES (numeric) and some selected SUPPORTING ATTRIBUTES (numeric).
+
+    # Feature Filtering.
+        # Variance Thresholding to remove low-variance features.
+        # Multicollinearity filter to remove highly correlated features. If Feature A and B have correlation > 0.95, drop one of them.
+
+    # Feature Selection.
+        # Reduce dataset to TOP-K features:
+        # Mutual Information to capture non-linear relationships (between features and target).
+        # Wrapper (RF Importance) to remove ignored features by model.
+        # ! PCA destroy feature interpretability, so we will avoid it for now. As we wanna interpret the features later with SHAP.
+
+    logger.info("=== STEP 03: COMPLETED SUCCESSFULLY ===\n")
 
 if __name__ == "__main__":
     main()
